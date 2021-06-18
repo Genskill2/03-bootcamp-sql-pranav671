47be1c12f794699da7daf6676e87e281b003ab88
Pranav Kadam <pranavkadam671@gmail.com>
2021-06-18 18:06:00 +0530

    N/A

--- delete1.sql
+++ delete1.sql
@@ -1,2 +1,2 @@
-DELETE from books_subjects where id in(select id from subjects where name = 'History');
+DELETE from books_subjects where subject in(select id from subjects where name = 'History');
 DELETE from subjects where name = 'History';
